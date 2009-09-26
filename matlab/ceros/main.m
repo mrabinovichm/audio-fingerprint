0017da579edd52fea79f9c9904e957f76f7250ae
mrabinovichm <mrabinovichm@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-09-26 17:02:00 +0000

    Se agrega la función dividet.m que selecciona un tramo del vector de tiempo. La función zhuella.m calcula la huella de una señal a partir de los cruces por cero en varios tramos de tiempo.

--- matlab/ceros/main.m
+++ matlab/ceros/main.m
@@ -1,22 +1,26 @@
-clear all;  %limpia variables
-close all;  %cierra ventanas
-clc;        %limpia pantalla
+clear all;          %limpia variables
+close all;          %cierra ventanas
+clc;                %limpia pantalla
 
-Fs = 100;         %frecuencia de muestreo en Hz. 
+Fs = 48000;         %frecuencia de muestreo en Hz. 
 Ts = 1/Fs;          %Periodo de muestreo en segundos.
+N = 6;              %Segundos del tramo del vector tiempo.
+tcorte = 1;         %tiempo de corte para la funcion dividet
 
-f = 7;            %frecuencia de la señal sinusoidal.
+f = 8000;           %frecuencia de la señal sinusoidal.
 fi = 0;             %argumento de la señal sinusoidal.
 
 %se genera un vector de tiempos a partir de la frecuencia de muestreo de longitud Fs.
-t=[0:Fs] * Ts;
+t=[0:N*Fs] * Ts;
 
 y = seno(f, t, fi);
-plot(t, y, '*-');
-title('Seno de frecuencia f Hz');
-xlabel('Tiempo (segundos)');
-ylabel('Amplitud');
-grid;
+% plot(t, y, '*-');
+% title('Seno de frecuencia f Hz');
+% xlabel('Tiempo (segundos)');
+% ylabel('Amplitud');
+% grid;
 
 z = zcr(y);         %z es el total de cruces por cero de la señal que se le pasa como argumento.
-disp(sprintf('Cruces por cero = %.0f', z))
+disp(sprintf('Cruces por cero (toda la senal) = %.0f', z))
+
+z = zhuella(N,tcorte,Fs,t,f,fi)
