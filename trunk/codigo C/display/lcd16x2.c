6fa74c394601a4585c86a59a9437f858e9f862c3
daicav <daicav@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-06 00:47:06 +0000

    Se agregaron las funciones "delay" e "inicialización del control del display"

--- trunk/codigo C/display/lcd16x2.c
+++ trunk/codigo C/display/lcd16x2.c
@@ -3,14 +3,14 @@
 /*Inicializa el puerto B en modo GPIO*/
 void init_gpio(void)
 {
-	HPCR.I.B.HGEN  = 1; /*  0: Host GPIO Port Enable                    */
+	HPCR.B.HGEN  = 1; /*  0: Host GPIO Port Enable                    */
 
-	HPCR.I.B.HA8EN = 0; /*  1: Host Address Line 8 Enable               */
-	HPCR.I.B.HA9EN = 0; /*  2: Host Address Line 9 Enable               */
-	HPCR.I.B.HCSEN = 0; /*  3: Host Chip Select Enable                  */
-	HPCR.I.B.HREN  = 0; /*  4: Host Request Enable                      */
-	HPCR.I.B.HAEN  = 0; /*  5: Host Acknowledge Enable                  */
-	HPCR.I.B.HEN   = 0; /*  6: Host Enable                              */
+	HPCR.B.HA8EN = 0; /*  1: Host Address Line 8 Enable               */
+	HPCR.B.HA9EN = 0; /*  2: Host Address Line 9 Enable               */
+	HPCR.B.HCSEN = 0; /*  3: Host Chip Select Enable                  */
+	HPCR.B.HREN  = 0; /*  4: Host Request Enable                      */
+	HPCR.B.HAEN  = 0; /*  5: Host Acknowledge Enable                  */
+	HPCR.B.HEN   = 0; /*  6: Host Enable                              */
 
 //Configuro bits de I/O como salidas
 	HDDR = 0xFFFF;
@@ -27,20 +27,47 @@ void write_lcd(char simbolo, char ctrl_dat)
 	HDR = dato.I;				 /*graba char en lcd*/ 
 }
 
-/*Inicializa el display LCD16x2*/
+
+void delay(int factor)
+{
+	int i, j;
+	
+	for(i = 0;  i < factor;  i++)     /* factor*100 us total delay:*/
+    {
+        for(j = 0;  j < (int)(100*CPUCLK/(2*1E6));  j++)   /*100 us*/
+        {
+            _nop();
+        }
+    }
+}
+
+/*Inicializa el display LCD16x2, pag.45 HD44780U.pdf*/
 void init_lcd(void)
 {
-	 
+	delay(150);  						/*esperar 15ms*/
+	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	delay(50);  						/*esperar 5ms*/
+	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	delay(1);  				    		/*esperar 100us*/
+	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	delay(1);  				    		/*esperar 100us*/
+	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	delay(1);  				    		/*esperar 100us*/
+	write_lcd(DPLY_OFF, CTRL);	    	/*comando Dply off*/
+	delay(1);  				    		/*esperar 100us*/
+	write_lcd(CLEAR, CTRL);	    		/*comando Clear Dply*/
+	delay(1);  				    		/*esperar 100us*/
+	write_lcd(ETY_MOD_SET, CTRL);	    /*comando Entry mode set*/
 }
 
 /*Envia un comando de configuracion al display*/
-void cmd_lcd(char *comando)
+void cmd_lcd(char comando)
 {
 
 }
 
-/*Envia los datos pertinentes a cmd_lcd*/
-char dato_lcd(char *datos)
+/*Envia los datos que se escribiran en el display*/
+void dato_lcd(char *datos)
 {
 
 }
--- trunk/codigo C/display/lcd16x2.h
+++ trunk/codigo C/display/lcd16x2.h
@@ -3,6 +3,10 @@
 
 #include <reg56303.h>
 
+#ifndef CPUCLK
+#define CPUCLK      67000000L   /* Hz */
+#endif
+
 #define CTRL 0x42
 #define DATO 0x62
 
@@ -36,20 +40,31 @@ typedef union
 
 } REGISTRO_HDR;
 
+#define CLEAR	      0x01 /*00000001 Borra Dply y vuelve cursor al inicio*/
+#define RET_HOME      0x02 /*00000010 Contador de direcciones a cero*/
+#define ETY_MOD_SET	  0x06 /*00000110 I/D=1 inc DDRAM, S=0 sin salto de Dply*/
+#define DPLY_ON		  0x0C /*00001100 D=1 Dply on, C=0 sin cursor, B=0 sin blink*/
+#define DPLY_OFF	  0x08 /*00001000 D=0 Dply off, C=0 sin cursor, B=0 sin blink*/
+#define CSOR_SHFT  	  0x18 /*00011000 */
+#define FUN_SET		  0x38 /*00111000 DL=1 8 bits, N=1 2_lineas, F=don't care*/
+
+
+/*Genera retardos multiplos de 100us*/
+void delay(int factor);
 
 /*Inicializa el puerto B en modo GPIO*/
 void init_gpio(void);
 
 /*Funcion que maneja la escritura en la memoria del lcd*/
-void write_lcd(char simbolo);
+void write_lcd(char simbolo, char ctrl_dat);
 
 /*Inicializa el display LCD16x2*/
 void init_lcd(void);
 
 /*Envia un comando de configuracion al display*/
-void cmd_lcd(char *comando);
+void cmd_lcd(char comando);
 
-/*Envia los datos pertinentes a cmd_lcd*/
-char dato_lcd(char *datos);
+/*Envia los datos que se escribiran en el display*/
+void dato_lcd(char *datos);
 
 #endif /*LCD16x2_H*/
