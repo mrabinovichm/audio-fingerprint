7c0ef1bf6ee2b28249e7f5a32ee0c1c7a851cc91
daicav <daicav@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-07 00:25:43 +0000

    Se agrega el main.c y se agregan funciones: "escribe datos al lcd", "lee datos del lcd", "verificación de que no esté ocupado el lcd"

--- codigo C/display/lcd16x2.c
+++ codigo C/display/lcd16x2.c
@@ -12,8 +12,8 @@ void init_gpio(void)
 	HPCR.B.HAEN  = 0; /*  5: Host Acknowledge Enable                  */
 	HPCR.B.HEN   = 0; /*  6: Host Enable                              */
 
-//Configuro bits de I/O como salidas
-	HDDR = 0xFFFF;
+
+	HDDR = 0xFFFF;	  /*Configuro bits de I/O como salidas*/	
 }
 
 void write_lcd(char simbolo, char ctrl_dat)
@@ -27,6 +27,22 @@ void write_lcd(char simbolo, char ctrl_dat)
 	HDR = dato.I;				 /*graba char en lcd*/ 
 }
 
+/*Lee un dato o el estado del Dply*/
+char read_lcd(char ctrl_dat)
+{
+	REGISTRO_HDR dato;
+	
+	HDDR = 0xFF00; 				 /*Configuro 8 bits de datos de I/O como entradas*/
+	dato.bytes.byte1 = ctrl_dat; /*R/!W=1; E=1; RS= 0(ctrl), 1(dato); BL encendido*/
+	HDR = dato.I;
+	dato.bits.bit1 = 0;		     /*flanco de bajada(E=0)*/
+	HDR = dato.I;
+	dato.I = HDR;				 /*lee dato desde lcd*/ 
+	
+	HDDR = 0xFFFF;				 /*Configuro bits de I/O como salidas*/
+
+	return dato.bytes.byte0;
+}
 
 void delay(int factor)
 {
@@ -45,29 +61,70 @@ void delay(int factor)
 void init_lcd(void)
 {
 	delay(150);  						/*esperar 15ms*/
-	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	write_lcd(FUN_SET, CTRL_WR);			/*comando Function set*/
 	delay(50);  						/*esperar 5ms*/
-	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	write_lcd(FUN_SET, CTRL_WR);			/*comando Function set*/
 	delay(1);  				    		/*esperar 100us*/
-	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	write_lcd(FUN_SET, CTRL_WR);			/*comando Function set*/
 	delay(1);  				    		/*esperar 100us*/
-	write_lcd(FUN_SET, CTRL);			/*comando Function set*/
+	write_lcd(FUN_SET, CTRL_WR);			/*comando Function set*/
 	delay(1);  				    		/*esperar 100us*/
-	write_lcd(DPLY_OFF, CTRL);	    	/*comando Dply off*/
+	write_lcd(DPLY_OFF, CTRL_WR);	    	/*comando Dply off*/
 	delay(1);  				    		/*esperar 100us*/
-	write_lcd(CLEAR, CTRL);	    		/*comando Clear Dply*/
+	write_lcd(CLEAR, CTRL_WR);	    		/*comando Clear Dply*/
 	delay(1);  				    		/*esperar 100us*/
-	write_lcd(ETY_MOD_SET, CTRL);	    /*comando Entry mode set*/
+	write_lcd(ETY_MOD_SET, CTRL_WR);	    /*comando Entry mode set*/
 }
 
-/*Envia un comando de configuracion al display*/
-void cmd_lcd(char comando)
-{
 
+/*Se fija si el Dply esta ocupado*/
+/* int busy(void)                                                                                                  */
+/* {                                                                                                               */
+/* 	int bf = 1;                                                                                                    */
+/* 	int i = 0;                                                                                                     */
+/* 	char status;                                                                                                   */
+/* 	status = read_lcd(CTRL_RD);                                                                                    */
+/* 	status = status & 0x80; 							/*me deja todo a 0 salvo el DB7 = BF*/                     
+/* 	if (status == 0x00)                                                                                            */
+/* 		return bf = 0;                                                                                             */
+/* 	else                                                                                                           */
+/* 		{                                                                                                          */
+/* 			while ((status != 0x00) || (i == 50000)) 	/*me quedo mientras este ocupado el lcd evitando el loop*/ 
+/* 				{                                                                                                  */
+/* 					status = read_lcd(CTRL_RD);                                                                    */
+/* 					status = status & 0x80; 			/*me deja todo a 0 salvo el DB7 = BF*/                     
+/* 					i++;                                                                                           */
+/* 				}                                                                                                  */
+/* 			if (status == 0x00)                                                                                    */
+/* 				return bf = 0;                                                                                     */
+/* 			return bf;                                                                                             */
+/* 		}	                                                                                                       */
+/*}                                                                                                                */
+
+
+/*Escribe una linea del Dply*/
+void wr_linea(char *data, int len_lin)
+{
+	int i;
+	for(i=0; i<len_lin; i++)
+	{
+		/*falta verificar ocupado*/
+		write_lcd(*(data+i), DATO_WR);
+	}
 }
 
 /*Envia los datos que se escribiran en el display*/
-void dato_lcd(char *datos)
+void dato_lcd(char *dato, int len_dato)
 {
-
+	if(len_dato <= LCD_16) 
+		{
+			wr_linea(dato, len_dato);   /*escribe solo una linea*/
+		}
+	else
+		{								/*escribe las dos lineas*/
+			wr_linea(dato, LCD_16);
+			/*busy*/
+			write_lcd(SDA_LIN, CTRL_WR);
+			wr_linea(dato, (len_dato - LCD_16));
+		}
 }
