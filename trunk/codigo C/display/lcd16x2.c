9099fde321a55b1fc21af6a92dad913638807f4e
mrabinovichm <mrabinovichm@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-13 22:48:22 +0000

    El módulo del display está funcionando.

--- trunk/codigo C/display/lcd16x2.c
+++ trunk/codigo C/display/lcd16x2.c
@@ -1,5 +1,8 @@
 #include "lcd16x2.h"
 
+//REGISTRO_HDR dato;
+
+
 /*Inicializa el puerto B en modo GPIO*/
 void init_gpio(void)
 {
@@ -16,33 +19,29 @@ void init_gpio(void)
 	HDDR = 0xFFFF;	  /*Configuro bits de I/O como salidas*/	
 }
 
-void write_lcd(char simbolo, char ctrl_dat)
+void write_lcd(unsigned char simbolo, short ctrl_dat)
 {
-	REGISTRO_HDR dato;
-
-	dato.bytes.byte0 = simbolo;  /*Caracter a escribir en lcd*/
-	dato.bytes.byte1 = ctrl_dat; /*R/!W=0; E=1; RS= 0(ctrl), 1(dato); BL encendido*/
-	HDR = dato.I;
-	dato.bits.bit1 = 0;		     /*flanco de bajada(E=0)*/
-	HDR = dato.I;				 /*graba char en lcd*/ 
+	HDR = simbolo;				 /*char a escribir en lcd*/
+	HDR |= ctrl_dat;			 /*palabra de control*/
+	HDR &= 0xFDFF;				 /*flanco de bajada(E=0)*/
 }
 
 /*Lee un dato o el estado del Dply*/
-char read_lcd(char ctrl_dat)                                                      
-{                                                                                 
- 	REGISTRO_HDR dato;                                                               
+//char read_lcd(char ctrl_dat)                                                      
+//{                                                                                 
+// 	REGISTRO_HDR dato;                                                               
  	                                                                                 
- 	HDDR = 0xFF00; 				 /*Configuro 8 bits de datos de I/O como entradas*/  
- 	dato.bytes.byte1 = ctrl_dat; /*R/!W=1; E=1; RS= 0(ctrl), 1(dato); BL encendido*/ 
- 	HDR = dato.I;                                                                    
- 	dato.bits.bit1 = 0;		     /*flanco de bajada(E=0)*/                           
- 	HDR = dato.I;                                                                    
- 	dato.I = HDR;				 /*lee dato desde lcd*/                              
+// 	HDDR = 0xFF00; 				 /*Configuro 8 bits de datos de I/O como entradas*/  
+// 	dato.bytes.byte1 = ctrl_dat; /*R/!W=1; E=1; RS= 0(ctrl), 1(dato); BL encendido*/ 
+// 	HDR = dato.I;                                                                    
+// 	dato.bits.bit1 = 0;		     /*flanco de bajada(E=0)*/                           
+// 	HDR = dato.I;                                                                    
+// 	dato.I = HDR;				 /*lee dato desde lcd*/                              
  	                                                                                 
- 	HDDR = 0xFFFF;				 /*Configuro bits de I/O como salidas*/              
+// 	HDDR = 0xFFFF;				 /*Configuro bits de I/O como salidas*/              
                                                                                    
- 	return dato.bytes.byte0;                                                         
-}                                                                                 
+// 	return dato.bytes.byte0;                                                         
+//}                                                                                 
 
 void delay(int factor)
 {
@@ -72,59 +71,62 @@ void init_lcd(void)
 	write_lcd(DPLY_OFF, CTRL_WR);	    	/*comando Dply off*/
 	delay(1);  				    		/*esperar 100us*/
 	write_lcd(CLEAR, CTRL_WR);	    		/*comando Clear Dply*/
-	delay(1);  				    		/*esperar 100us*/
+	delay(100);  				    	/*esperar 10ms*/
 	write_lcd(ETY_MOD_SET, CTRL_WR);	    /*comando Entry mode set*/
+	delay(1);							/*esperar 100us*/
+	write_lcd(DPLY_ON, CTRL_WR);	    	/*comando Dply on*/
+	delay(1);  				    		/*esperar 100us*/
 }
 
 
 /*Se fija si el Dply esta ocupado*/
-int busy(void)                                                                                                      
-{                                                                                                                   
-  	int bf = 1;                                                                                                     
- 	int i = 0;                                                                                                      
- 	char status;                                                                                                    
-  	status = read_lcd(CTRL_RD);                                                                                     
- 	status = status & 0x80; 							/*me deja todo a 0 salvo el DB7 = BF*/                          
-	if (status == 0x00)                                                                                             
-  		return bf = 0;                                                                                              
-  	else                                                                                                            
-  		{                                                                                                           
-  			while ((status != 0x00) || (i == 50000)) 	/*me quedo mientras este ocupado el lcd evitando el loop*/  
- 				{                                                                                                      
-  					status = read_lcd(CTRL_RD);                                                                     
-  					status = status & 0x80; 			/*me deja todo a 0 salvo el DB7 = BF*/                      
-  					i++;                                                                                            
-  				}                                                                                                   
-  			if (status == 0x00)                                                                                     
-  				return bf = 0;                                                                                      
-  			return bf;                                                                                              
-  		}	                                                                                                        
-}                                                                                                                    
+//int busy(void)                                                                                                      
+//{                                                                                                                   
+//  	int bf = 1;                                                                                                     
+// 	int i = 0;                                                                                                      
+// 	char status;                                                                                                    
+// 	status = read_lcd(CTRL_RD);                                                                                     
+// 	status = status & 0x80; 							/*me deja todo a 0 salvo el DB7 = BF*/                          
+//	if (status == 0x00)                                                                                             
+// 		return bf = 0;                                                                                              
+// 	else                                                                                                            
+// 		{                                                                                                           
+// 			while ((status != 0x00) || (i == 50000)) 	/*me quedo mientras este ocupado el lcd evitando el loop*/  
+// 				{                                                                                                      
+// 					status = read_lcd(CTRL_RD);                                                                     
+// 					status = status & 0x80; 			/*me deja todo a 0 salvo el DB7 = BF*/                      
+// 					i++;                                                                                            
+// 				}                                                                                                   
+// 			if (status == 0x00)                                                                                     
+// 				return bf = 0;                                                                                      
+// 			return bf;                                                                                              
+// 		}	                                                                                                        
+//}                                                                                                                    
                                                                                                                       
                                                                                                                       
 /*Escribe una linea del Dply*/                                                                                       
-void wr_linea(char *data, int len_lin)                                                                               
-{                                                                                                                    
- 	int i;                                                                                                              
- 	for(i=0; i<len_lin; i++)                                                                                            
- 	{                                                                                                                   
+//void wr_linea(char *data, int len_lin)                                                                               
+//{                                                                                                                    
+// 	int i;                                                                                                              
+// 	for(i=0; i<len_lin; i++)                                                                                            
+// 	{                                                                                                                   
  		/*falta verificar ocupado*/                                                                                     
- 		write_lcd(*(data+i), DATO_WR);                                                                                  
- 	}                                                                                                                   
-}                                                                                                                    
+// 		write_lcd(*(data+i), DATO_WR);                                                                                  
+// 	}                                                                                                                   
+//}                                                                                                                    
                                                                                                                       
 /*Envia los datos que se escribiran en el display*/                                                                  
-void dato_lcd(char *dato, int len_dato)                                                                              
-{                                                                                                                    
-	if(len_dato <= LCD_16)                                                                                              
-		{                                                                                                               
- 			wr_linea(dato, len_dato);   /*escribe solo una linea*/                                                      
- 		}                                                                                                               
- 	else                                                                                                                
- 		{								/*escribe las dos lineas*/                                                      
- 			wr_linea(dato, LCD_16);                                                                                     
+//void dato_lcd(char *dato, int len_dato)                                                                              
+//{                                                                                                                    
+//	if(len_dato <= LCD_16)                                                                                              
+//		{                                                                                                               
+// 			wr_linea(dato, len_dato);   /*escribe solo una linea*/                                                      
+// 		}                                                                                                               
+// 	else                                                                                                                
+// 		{								/*escribe las dos lineas*/                                                      
+// 			wr_linea(dato, LCD_16);                                                                                     
 			/*busy*/                                                                                                    
- 			write_lcd(SDA_LIN, CTRL_WR);                                                                                
- 			wr_linea(dato, (len_dato - LCD_16));                                                                        
- 		}                                                                                                            
-}                                                                                                                    
+// 			write_lcd(SDA_LIN, CTRL_WR);                                                                                
+// 			wr_linea(dato, (len_dato - LCD_16));                                                                        
+// 		}                                                                                                            
+//}                                                                                                                    
