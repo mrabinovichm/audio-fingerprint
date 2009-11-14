fafdf4e7efbeb0d4669f24154a738611aa1baa74
daicav <daicav@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-14 21:38:04 +0000

    Display completamente terminado.

--- trunk/codigo C/display/lcd16x2.h
+++ trunk/codigo C/display/lcd16x2.h
@@ -8,11 +8,15 @@
 #define CPUCLK      67000000L   /* Hz */
 #endif
 
+#define LCD_16 16	   /*Long de una linea del Dply*/
+
+/*Lineas de control RS, R/W, E y Backlight*/
 #define CTRL_WR 0x4200 /*x10xx01x BL=1(backlight on), RS=0, R/W=0, E=1*/
 #define CTRL_RD 0x4600 /*x10xx11x BL=1(backlight on), RS=0, R/W=1, E=1*/
 #define DATO_WR	0x6200 /*x11xx01x BL=1(backlight on), RS=1, R/W=0, E=1*/
 #define DATO_RD	0x6600 /*x11xx11x BL=1(backlight on), RS=1, R/W=1, E=1*/
 
+/*Palabras de control del LCD16x2*/
 #define CLEAR	      0x01 /*00000001 Borra Dply y vuelve cursor al inicio*/
 #define RET_HOME      0x02 /*00000010 Contador de direcciones a cero*/
 #define ETY_MOD_SET	  0x06 /*00000110 I/D=1 inc DDRAM, S=0 sin salto de Dply*/
@@ -22,37 +26,36 @@
 #define FUN_SET		  0x38 /*00111000 DL=1 8 bits, N=1 2_lineas, F=don't care*/
 #define SDA_LIN		  0xC0 /*11000000 Posiciona el cursor en la 2da linea*/
 
-#define LCD_16 16	 /*Long de una linea del Dply*/ 
 
-typedef union 
-{
-	struct{
-			int bit0  : 1;
-			int bit1  : 1;
-			int bit2  : 1;
-			int bit3  : 1;
-			int bit4  : 1;
-			int bit5  : 1;
-			int bit6  : 1;
-			int bit7  : 1;
-			int bit8  : 1;
-			int bit9  : 1;
-			int bit10 : 1;
-			int bit11 : 1;
-			int bit12 : 1;
-			int bit13 : 1;
-			int bit14 : 1;
-			int bit15 : 1;
-	} bits;
-
-	struct{
-			int byte0  : 8;
-			int byte1  : 8;
-	} bytes;
-
-	int I;
-
-} REGISTRO_HDR;
+/* typedef union            */
+/* {                        */
+/* 	struct{                 */
+/* 			int bit0  : 1;  */
+/* 			int bit1  : 1;  */
+/* 			int bit2  : 1;  */
+/* 			int bit3  : 1;  */
+/* 			int bit4  : 1;  */
+/* 			int bit5  : 1;  */
+/* 			int bit6  : 1;  */
+/* 			int bit7  : 1;  */
+/* 			int bit8  : 1;  */
+/* 			int bit9  : 1;  */
+/* 			int bit10 : 1;  */
+/* 			int bit11 : 1;  */
+/* 			int bit12 : 1;  */
+/* 			int bit13 : 1;  */
+/* 			int bit14 : 1;  */
+/* 			int bit15 : 1;  */
+/* 	} bits;                 */
+/*                          */
+/* 	struct{                 */
+/* 			int byte0  : 8; */
+/* 			int byte1  : 8; */
+/* 	} bytes;                */
+/*                          */
+/* 	int I;                  */
+/*                          */
+/* } REGISTRO_HDR;          */
 
 
 /*Inicializa el puerto B en modo GPIO*/
@@ -73,10 +76,7 @@ void init_lcd(void);
 /*Se fija si el Dply esta ocupado*/
 //int busy(void);
 
-/*Escribe una linea del Dply*/
-//void wr_linea(char *data, int len_lin);
-
 /*Envia los datos que se escribiran en el display*/
-//void dato_lcd(char *dato, int len_dato);
+void dato_lcd(unsigned char *dato, short len);
 
 #endif /*LCD16x2_H*/
