4b1a4ad873ad8b3a567137859ca25529f488c4c2
edgardovaz <edgardovaz@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-16 01:56:58 +0000

    La definición de los textos a desplegar en el display pasan dentro del main. Para compilar este código con Tasking, deshabilitar la opción de optimizar(elegir "no optimization"), de lo contrario la función delay() no se ejecutará correctamente ya que los nops no se tendrán en cuenta. Esto último hace que no se cumplan los tiempos de espera y por tanto los mensajes mostrados en el display no son coherentes.

--- trunk/codigo C/display/main.c
+++ trunk/codigo C/display/main.c
@@ -1,19 +1,19 @@
 #include "lcd16x2.h"
 
-unsigned char dsp_afp[]   = "DSP 2009        AUDIOFINGERPRINT";
 
-unsigned char busq[]      = "Buscando...";
-unsigned char h_enc[]     = "Huella          encontrada";
-unsigned char h_no_enc[]  = "Huella          no encontrada";
-
-unsigned char enya[]      = "Enya: Only Time";
-unsigned char oasis[]     = "Oasis:          Headshrinker";
-unsigned char van_halen[] = "Van Halen:      Erupsion";
-
-void dato_lcd(unsigned char *dato, short len_dato);
 
 int main()
 {
+	unsigned char dsp_afp[]   = "DSP 2009        AUDIOFINGERPRINT";
+
+    unsigned char busq[]      = "Buscando...";
+    unsigned char h_enc[]     = "Huella          encontrada";
+	unsigned char h_no_enc[]  = "Huella          no encontrada";
+
+	unsigned char enya[]      = "Enya: Only Time";
+	unsigned char oasis[]     = "Oasis:          Headshrinker";
+	unsigned char van_halen[] = "Van Halen:      Erupsion";
+
 	short len;
 
 	init_gpio();	
@@ -21,21 +21,27 @@ int main()
 
   	len = strlen((char *)dsp_afp);
 	dato_lcd(dsp_afp, len);
-	delay(10000);
+	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
   	len = strlen((char *)enya);
 	dato_lcd(enya, len);
-	delay(10000);
+	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
   	len = strlen((char *)h_no_enc); 		                   
 	dato_lcd(h_no_enc, len);
-	delay(10000);
+	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
   	len = strlen((char *)busq); 		                   
 	dato_lcd(busq, len);
+	delay(3000);
+	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
+	delay(100);  				    	/*esperar 10ms*/
+	write_lcd(DPLY_OFF, CTRL_WR);
+	delay(1);  				    		/*esperar 100us*/
+	HDR = 0x0000;
 
 }/*Fin main*/
 
