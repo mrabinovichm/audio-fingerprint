8244ec13d3d032f53dac509baab4c6dd13e69a60
edgardovaz <edgardovaz@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-11-22 00:29:24 +0000

    Se modificó la isr para recibir datos desde el codec, un puntero dentro de la misma guarda los datos en uno de tres posibles buffers los cuales se seleccionan mediante una bandera desde la tarea ppal.

--- codigo C/codec/main.c
+++ codigo C/codec/main.c
@@ -1,22 +1,38 @@
 #include "cs4215.h"
 #include "loopback.h"
+#include "int.h"
 
-extern _circ _fract buffer_in[LARGO];
-extern _circ _fract buffer_in2[LARGO];
+_circ _fract buffer_in0[S];
+_circ _fract buffer_in1[S];
+_circ _fract buffer_in2[S];
 extern _circ _fract *ptr_buffer;
+_fract *ptr_fin;
 
-char selec_buf = OFF;
 
 int main(void)
 {
-	ptr_buffer = buffer_in;
-	ssi_init(); 				//inicializa codec
-    enable_interrupts();		//establece la prioridad del micro
-
+   	short bandera = 0;			  //valores de bandera 0, 1, o 2
 	
+	inicializacion();			  //inicializa el TIO0
+	ssi_init(); 				  //inicializa codec
+    enable_interrupts();
+    		
 	for(;;) 
 	{
-
+		switch(bandera)
+		{
+			case 0 : ptr_buffer = &buffer_in0;
+					 break;
+			case 1 : ptr_buffer = &buffer_in1;
+					 break;
+			case 2 : ptr_buffer = &buffer_in2;
+					 break;
+			default: break;
+		}
+		ptr_fin = ptr_buffer + S-1;
+		while(ptr_buffer <= ptr_fin);
+		bandera++;
+		bandera = bandera % 3;
 	}
 
 }
