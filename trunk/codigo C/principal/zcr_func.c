5e2dff10f9cc60490ef35643e1eefdaabdd0c847
mrabinovichm <mrabinovichm@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-12-03 16:17:01 +0000

    Se implementa la función de búsqueda, que usa la función correlación (también implementada) que a su vez usa la función promedio (implementada). Compila.

--- trunk/codigo C/principal/zcr_func.c
+++ trunk/codigo C/principal/zcr_func.c
@@ -56,13 +56,105 @@ short fundamentales(int *ceros)
 //**********************************************************************************************************
 
 
+/* ******************************************************************************************************** */
+/* ********* Funcion que halla el promedio ******************************************** */
+/* ******************************************************************************************************** */
+
+float promedio(float *p, int largo)
+{
+	int i;
+	float suma, prom;
+	
+	suma = 0;
+
+	for (i=0;i<largo;i++) suma += *(p+i);
+	
+	prom = (float)(suma/largo);
+
+	return prom;
+}
+
+/* ******************************************************************************************************** */
+/* ********* Funcion que halla la correlacion entre dos señales******************************************** */
+/* ******************************************************************************************************** */
+
+short correlacion(float *px,float *py)
+{                                                                   
+ 	_fract r;                                                           	
+	float mx,my,suma,sumax,sumay,sigmax,sigmay,div;                                                       
+ 	int k,m;                                                           
+ 	float *px0,*py0,*finx,*finy;
+	
+	int largox = q;
+	int largoy = 2*q;
+  
+ 	finx = px + largox;                                            
+ 	finy = py + largoy;                                                
+    
+ 	px0 = px;                                                          
+ 	py0 = py;                                                          
+   
+ 	/* calculo de la media de las señales */
+ 	mx = promedio(px0, largox); 	   
+ 	my = promedio(py0, largoy);                                           
+  
+ 	/* calculo de señales sin valor medio */
+ 	while(px0 != finx)                                                 
+ 	{                                                                  
+ 		*px0 -= mx;                                                    
+ 		px0++;                                                         
+ 	}                                                                  
+   
+ 	while(py0 != finy)                                                 
+ 	{                                                                  
+ 		*py0 -= my;                                                    
+ 		py0++;                                                         
+ 	}                                                                  
+    
+     for (k=0; k<largox; k++) 
+     	sumax += (*(px+k)) * (*(px+k));                                                                    
+
+ 	/* cuentas */
+ 	for (m=0; m<largoy-largox; m++)                                      
+    {                                                              
+		sumax   = 0;                                               
+     	sumay   = 0;                                               
+     	sigmax  = 0;                                               
+     	sigmay  = 0;                                               
+     	suma    = 0;                                               
+     	for (k=0; k<largox; k++)                                             
+       	{                                                      
+       		suma += (*(px+k)) * (*(py+k+m));                   
+       		/* ******************** */
+       	   	sumay += (*(py+k+m)) * (*(py+k+m));                            
+		}                                                          
+    }                                                              
+	/* ******************** */
+    sigmax = sqrt(sumax/largox);                                   
+    sigmay = sqrt(sumay/largox);                                   
+    /* ******************** */                                     
+    div = sigmax*sigmay*largox;                                        
+    r = (_fract)(suma/div);
+	
+	if (fabs(r)>=0.9) return 1;
+	
+	return 0;
+}
+/* ******************************************************************************************************** */
+
+
 //**********************************************************************************************************
 //**************** Funcion que busca la huella pasada como parametro en la base de datos *******************
 //**********************************************************************************************************
 
-short busqueda(float *h)
+short busqueda(float *h, float **base_de_datos, short tam_bd)
 {
-	h++; /*esta linea hay que sustituirla por el codigo que falta implementar*/
-	return 0;
+	short i;
+    for (i=0; i<tam_bd; i++)
+    {
+        if (correlacion(h, base_de_datos[i]) == 1)
+        	return i; 								/* índice en donde está el mensaje para el lcd */
+    }
+    return -1; 										                 /* si no la encontró va un -1 */
 }
-//**********************************************************************************************************
+/* ******************************************************************************************************* */
