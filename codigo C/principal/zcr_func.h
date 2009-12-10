1f857665165b829746103aa7bc75bdecce481944
daicav <daicav@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-12-10 00:02:10 +0000

    Se solucionan errores en zcr_func.c

--- codigo C/principal/zcr_func.h
+++ codigo C/principal/zcr_func.h
@@ -4,10 +4,10 @@
 #define 	tr_g 	0.015                   	/*intervalo de ventana de calculo*/
 #define 	tr_c 	0.005                    	/*intervalo de desplazamiento de la ventana*/
 #define 	L 		3                         	/*3 segundos de señal para generar su huella*/
-#define 	umbral	0.2					   		/*umbral de decision al comparar huellas*/
+#define 	UMBRAL	0.9					   		/*umbral de decision al comparar huellas*/
 #define 	W		720							/*cantidad de muestras equivalentes a tr_g segundos*/
 #define 	S		240							/*cantidad de muestras equivalentes a tr_c segundos*/
-#define  	q		1197
+#define  	q		597
 #define 	T 		3							/*cantidad de tramos de 5ms*/
 
 
