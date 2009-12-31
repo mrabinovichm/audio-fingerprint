9b58a2e43aabd84946a8141bb32552110e393f9e
daicav <daicav@1268bf14-aa27-11de-a86f-b10fb6d3a35f>
2009-12-31 00:16:42 +0000

    Modificación de prueba.c en el módulo del display para quedara funcinando correctamente. Se quitan archivos de respaldo del diseño pcb (.bck) y se agrega el archivo gerber LCD16x2-Cobre.pho. Se agregan referencias faltantes en la literatura a leer. Se agregan las opciones de proyecto del compilador Tasking EDE

--- codigo C/display/prueba.c
+++ codigo C/display/prueba.c
@@ -1,41 +1,47 @@
 #include "lcd16x2.h"
 
+#define LARGO 	32
 
+	 _Y unsigned char dsp_afp[]    = "DSP 2009        AUDIOFINGERPRINT";
+	 _Y unsigned char sw_2[]       = "Presione SW2    para  comenzar  ";
+	 _Y unsigned char busq[]       = "Buscando        huella...       ";
+     _Y unsigned char h_enc[]      = "Huella          encontrada      ";
+	 _Y unsigned char h_no_enc[]   = "Huella          no encontrada   ";
+ 	 _Y unsigned char error[]      = "Error                           ";
+ 	 
+ 	 _Y unsigned char enya[]       = "Enya:           Only Time       ";
+	 _Y unsigned char oasis[]      = "Oasis:          Headshrinker    ";
+	 _Y unsigned char van_halen[]  = "Van Halen:      Eruption        ";
+	 _Y unsigned char losolima[]   = "Los Olima       Los dos Gallos  ";
+	 _Y unsigned char zitarrosa[]  = "Zitarrosa:      A Jose Artigas  ";
+	 _Y unsigned char curtidores[] = "Curtidores:     Cuple IMM       ";
+	 _Y unsigned char mozart[]     = "Mozart:         Marcha Turca    ";
+	 _Y unsigned char sodastereo[] = "Soda Stereo:    De Musica Ligera";
 
 int main()
 {
-	unsigned char dsp_afp[]   = "DSP 2009        AUDIOFINGERPRINT";
-
-    unsigned char busq[]      = "Buscando...";
-    unsigned char h_enc[]     = "Huella          encontrada";
-	unsigned char h_no_enc[]  = "Huella          no encontrada";
-
-	unsigned char enya[]      = "Enya: Only Time";
-	unsigned char oasis[]     = "Oasis:          Headshrinker";
-	unsigned char van_halen[] = "Van Halen:      Eruption";
-
-	short len;
+   	//short len;
 
 	init_gpio();	
 	init_lcd();
 
-  	len = strlen((char *)dsp_afp);
-	dato_lcd(dsp_afp, len);
+  	//len = strlen((char *)dsp_afp);
+	dato_lcd(dsp_afp, LARGO);
 	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
-  	len = strlen((char *)enya);
-	dato_lcd(enya, len);
+  	//len = strlen((char *)enya);
+	dato_lcd(enya, LARGO);
 	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
-  	len = strlen((char *)h_no_enc); 		                   
-	dato_lcd(h_no_enc, len);
+  	//len = strlen((char *)h_no_enc); 		                   
+	dato_lcd(h_no_enc, LARGO);
 	delay(1000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
-  	len = strlen((char *)busq); 		                   
-	dato_lcd(busq, len);
+  	//len = strlen((char *)busq); 		                   
+	dato_lcd(busq, LARGO);
 	delay(3000);
 	write_lcd(CLEAR, CTRL_WR);	    	/*comando Clear Dply*/
 	delay(100);  				    	/*esperar 10ms*/
